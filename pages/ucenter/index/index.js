0123992e2c5055a6368b6b891cf6815aa13ef6ee
Limanman507 <73656941+Limanman507@users.noreply.github.com>
2021-05-06 15:02:11 +0800

    Add files via upload

--- pages/ucenter/index/index.js
+++ pages/ucenter/index/index.js
@@ -1,98 +1,296 @@
-// pages/index/index.js
-//登陆
-var util = require('../../../utils/util.js');
-var api = require('../../../config/api.js');
-var user = require('../../../services/user.js');
-
 const app = getApp()
+const AUTH = require('../../../utils/auth')
+const WXAPI = require('apifm-wxapi')
 
 Page({
-    data: {
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        status: {},
-    },
-    goProfile: function (e) {
-        let res = util.loginNow();
-        if (res == true) {
-            wx.navigateTo({
-                url: '/pages/ucenter/settings/index',
-            });
-        }
-    },
-    toOrderListTap: function(event) {
-        let res = util.loginNow();
-        if (res == true) {
-            let showType = event.currentTarget.dataset.index;
-            wx.setStorageSync('showType', showType);
-            wx.navigateTo({
-                url: '/pages/ucenter/order-list/index?showType=' + showType,
-            });
-        }
-    },
-    toAddressList: function(e) {
-        let res = util.loginNow();
-        if (res == true) {
-            wx.navigateTo({
-                url: '/pages/ucenter/address/index?type=0',
-            });
-        }
-    },
-    toAbout: function () {
-        wx.navigateTo({
-            url: '/pages/ucenter/about/index',
-        });
-    },
-    toFootprint: function(e) {
-        let res = util.loginNow();
-        if (res == true) {
-            wx.navigateTo({
-                url: '/pages/ucenter/footprint/index',
-            });
-        }
-    },
-    goAuth: function(e) {
-        wx.navigateTo({
-            url: '/pages/app-auth/index',
-        });
-    },
-    onLoad: function(options) {
-    },
-    onShow: function() {
-        let userInfo = wx.getStorageSync('userInfo');
-        if(userInfo == ''){
-            this.setData({
-                hasUserInfo: 0,
-            });
-        }
-        else{
-            this.setData({
-                hasUserInfo: 1,
-            });
-        }
-        this.setData({
-            userInfo: userInfo,
-        });
-        this.getOrderInfo();
-        wx.removeStorageSync('categoryId');
-    },
+  data: {
+    wxlogin: true,
 
-    onPullDownRefresh: function() {
-        wx.showNavigationBarLoading()
-        this.getOrderInfo();
-        wx.hideNavigationBarLoading() //完成停止加载
-        wx.stopPullDownRefresh() //停止下拉刷新
-    },
-    getOrderInfo: function(e) {
-        let that = this;
-        util.request(api.OrderCountInfo).then(function(res) {
-            if (res.errno === 0) {
-                let status = res.data;
-                that.setData({
-                    status: status
-                });
-            }
-        });
-    },
+    aboutUsTitle: '',
+    aboutUsContent: '',
+    servicePhoneNumber: '',
+    balance: 0,
+    freeze: 0,
+    score: 0,
+    score_sign_continuous: 0,
+    iconSize: 45,
+    iconColor: '#999999'
+  },
+  onPullDownRefresh: function () {
+    var that = this
+    wx.showNavigationBarLoading()
+    that.onShow()
+    wx.hideNavigationBarLoading() //完成停止加载
+    wx.stopPullDownRefresh() //停止下拉刷新
+  },
+  onLoad() {
+    this.setData({
+      version: app.globalData.version,
+      background_color: app.globalData.globalBGColor,
+      bgRed: app.globalData.bgRed,
+      bgGreen: app.globalData.bgGreen,
+      bgBlue: app.globalData.bgBlue,
+      logo: wx.getStorageSync('logo')
+    })
+  },
+  onShow() {
+    var that = this;
+    that.getAboutUs();
+    that.getservicePhoneNumber();
+
+    AUTH.checkHasLogined().then(isLogined => {
+      this.setData({
+        wxlogin: isLogined
+      })
+      if (isLogined) {
+        this.getUserApiInfo();
+        this.getUserAmount();
+        this.checkScoreSign();
+      }
+    })
+    
+  },
+  aboutUs: function () {
+    var that = this
+    wx.showModal({
+      title: that.data.aboutUsTitle,
+      content: that.data.aboutUsContent,
+      showCancel: false
+    })
+  },
+  makePhoneCall: function () {
+    var that = this;
+    wx.makePhoneCall({
+      phoneNumber: that.data.servicePhoneNumber,
+      success: function (res) { },
+      fail: function (res) {
+        wx.showModal({
+          title: '呼叫失败',
+          content: '请稍后再试',
+          showCancel: false,
+        })
+      },
+      complete: function (res) { },
+    })
+  },
+  getPhoneNumber: function (e) {
+    if (!e.detail.errMsg || e.detail.errMsg != "getPhoneNumber:ok") {
+      console.log(e.detail.errMsg)
+      wx.showModal({
+        title: '提示',
+        content: '无法获取手机号码',
+        showCancel: false
+      })
+      return;
+    }
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/wxapp/bindMobile',
+      data: {
+        token: wx.getStorageSync('token'),
+        encryptedData: e.detail.encryptedData,
+        iv: e.detail.iv
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          wx.showToast({
+            title: '绑定成功',
+            icon: 'success',
+            duration: 2000
+          })
+          that.getUserApiInfo();
+        } else {
+          wx.showModal({
+            title: '提示',
+            content: '绑定失败',
+            showCancel: false
+          })
+        }
+      }
+    })
+  },
+  getUserApiInfo: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/detail',
+      data: {
+        token: wx.getStorageSync('token')
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            apiUserInfoMap: res.data.data,
+            userMobile: res.data.data.base.mobile
+          });
+        }
+      }
+    })
+
+  },
+  getUserAmount: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/amount',
+      data: {
+        token: wx.getStorageSync('token')
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            balance: res.data.data.balance,
+            freeze: res.data.data.freeze,
+            score: res.data.data.score
+          });
+        }
+      }
+    })
+
+  },
+  getAboutUs: function () {
+    var that = this
+    //  获取关于我们Title
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/config/get-value',
+      data: {
+        key: 'aboutUsTitle'
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            aboutUsTitle: res.data.data.value
+          })
+        }
+      }
+    })
+    //  获取关于我们内容
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/config/get-value',
+      data: {
+        key: 'aboutUsContent'
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            aboutUsContent: res.data.data.value
+          })
+        }
+      }
+    })
+  },
+  getservicePhoneNumber: function () {
+    var that = this
+    //  获取客服电话
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/config/get-value',
+      data: {
+        key: 'servicePhoneNumber'
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            servicePhoneNumber: res.data.data.value
+          })
+        }
+      }
+    })
+  },
+  checkScoreSign: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/score/today-signed',
+      data: {
+        token: wx.getStorageSync('token')
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            score_sign_continuous: res.data.data.continuous
+          });
+        }
+      }
+    })
+  },
+  scoresign: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/score/sign',
+      data: {
+        token: wx.getStorageSync('token')
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.getUserAmount();
+          that.checkScoreSign();
+        } else {
+          wx.showModal({
+            title: '错误',
+            content: res.data.msg,
+            showCancel: false
+          })
+        }
+      }
+    })
+  },
+
+  cancelLogin() {
+    this.setData({
+      wxlogin: true
+    })
+  },
+  scanOrder(){
+    wx.scanCode({
+      onlyFromCamera: true,
+      success(res) {
+        wx.navigateTo({
+          url: '/pages/order-details/scan-result?id=' + res.result,
+        })
+      },
+      fail(err){
+        console.error(err)
+        wx.showToast({
+          title: err.errMsg,
+          icon: 'none'
+        })
+      }
+    })
+  },
+  updateUserInfo(e) {
+    wx.getUserProfile({
+      lang: 'zh_CN',
+      desc: '用于完善会员资料',
+      success: res => {
+        console.log(res);
+        this._updateUserInfo(res.userInfo)
+      },
+      fail: err => {
+        console.log(err);
+        wx.showToast({
+          title: err.errMsg,
+          icon: 'none'
+        })
+      }
+    })
+  },
+  async _updateUserInfo(userInfo) {
+    const postData = {
+      token: wx.getStorageSync('token'),
+      nick: userInfo.nickName,
+      avatarUrl: userInfo.avatarUrl,
+      city: userInfo.city,
+      province: userInfo.province,
+      gender: userInfo.gender,
+    }
+    const res = await WXAPI.modifyUserInfo(postData)
+    if (res.code != 0) {
+      wx.showToast({
+        title: res.msg,
+        icon: 'none'
+      })
+      return
+    }
+    wx.showToast({
+      title: '登陆成功',
+    })
+    this.getUserApiInfo()
+  },
 })
